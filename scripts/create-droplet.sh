c0e1be29ed956af2bf441129b3e978216daf4e63
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-26 22:56:44 -0300

    some fixes

--- scripts/create-droplet.sh
+++ scripts/create-droplet.sh
@@ -6,5 +6,6 @@ doctl compute droplet create \
     --size s-1vcpu-1gb \
     --region nyc1 \
     --vpc-uuid ba5f81a6-5df2-4149-ad14-10cb12440d95 \
+    --ssh-keys 43405929 \
     my-burger-api-gtw
 
