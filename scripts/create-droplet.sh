7effd39d24144a61715c89ccb3efc592bb612383
FlorencioFer <fur_oliveira@hotmail.com>
2024-09-26 21:49:26 -0300

    changing action

--- scripts/create-droplet.sh
+++ scripts/create-droplet.sh
@@ -6,10 +6,5 @@ doctl compute droplet create \
     --size s-1vcpu-1gb \
     --region nyc1 \
     --vpc-uuid ba5f81a6-5df2-4149-ad14-10cb12440d95 \
-    kong-api-gtw
+    my-burger-api-gtw
 
-# Copiar a pasta inteira para o Droplet
-doctl compute scp ../src kong-api-gtw:/root/src
-
-# SSH para o Droplet e executar o docker-compose
-doctl compute ssh kong-api-gtw --ssh-command "cd /root/src && docker-compose up -d"
